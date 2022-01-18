93267428a91a17ef6bdc5da76ffa4ce1ab441277
amitparekh01 <72241433+amitparekh01@users.noreply.github.com>
2022-01-18 16:32:04 +0900

    f commit

---
+++ src/test/java/AITest/DemoAI/MyMojoTest.java
@@ -0,0 +1,65 @@
+package AITest.DemoAI;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import com.epam.healenium.SelfHealingDriver;
+
+public class MyMojoTest {
+	
+	
+	//public static String remote_url_chrome = "http://localhost:4445/wd/hub";
+	public WebDriver driver;
+	
+	
+	  
+	  @BeforeMethod
+	public void testInit() {
+		  //System.setProperty("webdriver.chrome.driver","src/main/resources/selenium.driver/chromedriver");
+		  WebDriver delegate = new ChromeDriver(); // declare delegate
+			 driver = SelfHealingDriver.create(delegate); // create Self-healing driver
+		}
+	
+	
+  @Test
+  public void f() throws InterruptedException {
+	  
+	
+		 
+	  
+	  
+      driver=new ChromeDriver();
+	  
+     
+	  driver.get("https://staging.k8s-sailor.com/dashboard/login");
+	  
+	  
+	  //re.capTest(); 
+	  Thread.sleep(500);
+	  driver.findElement(By.name("email")).sendKeys("parekh.amit@zeals.co.jp");
+	  //re.check(driver, "login-check");
+	  //re.capTest();  // To make a test fail in case of difference
+	  
+	  
+	  
+	  
+  }
+  @AfterMethod
+  public void afterMethod() {
+	  //driver.quit();
+	 // re.cap();
+	  
+  }
+  
+}
